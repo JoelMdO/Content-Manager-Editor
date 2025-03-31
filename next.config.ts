3e2a3278b92afb7fa14d695d0083e123cc3b27ad
JoelMdO <joel.montesdeoca@gmail.com>
2025-03-31 17:53:31 +0300

    [Version 0.0.6]

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,64 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+import TerserPlugin from 'terser-webpack-plugin';
 
-const nextConfig: NextConfig = {
+const pwaNextConfig = withPWA({
+    dest: "public",
+    disable: true,
+    register: true,
+    skipWaiting: true,
+});
+//
+
+const nextConfig = {    
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // Apply these headers to all routes
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "no-referrer",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload",
+                    },
+                    {   key: "Cross-Origin-Opener-Policy", 
+                        value: "same-origin" 
+                    },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Methods", value: process.env.NEXT_PUBLIC_ALLOWED_METHODS!},
+                    { key: "Access-Control-Allow-Headers", value: process.env.NEXT_PUBLIC_ALLOWED_HEADERS!}
+                ],
+            },
+        ];
+    },
+webpack: (config: any) => { 
+    config.resolve.fallback = { 
+        ...config.resolve.fallback, 
+        stream: "stream-browserify", 
+        util: "util/", 
+    }; 
+
+        config.optimization = { 
+            minimize: true, 
+            minimizer: [ 
+                new TerserPlugin({ 
+            terserOptions: { 
+                compress: { drop_console: true, }, 
+        }, }), 
+    ], };
+        return config; 
+}, 
 };
 
-export default nextConfig;
+
+export default pwaNextConfig(nextConfig);
