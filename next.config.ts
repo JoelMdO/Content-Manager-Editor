22803c1de2129f6d26a4cfd7df74e4a8faf83d7c
JoelMdO <joel.montesdeoca@gmail.com>
2025-03-31 07:53:16 +0300

    [Added] Docs for github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static export
+  distDir: 'docs',  // Outputs files to /docs for GitHub Pages
+  basePath: '/CMS', // Set base path to match the repo name
+  assetPrefix: '/CMS/', // Prefix assets for GitHub Pages
 };
 
 export default nextConfig;
