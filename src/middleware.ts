2e934d19ec9ea02465f8f5be9a1d8c846f15fc33
JoelMdO <joel.montesdeoca@gmail.com>
2025-04-03 02:17:24 +0300

    <fix>[Middleware]: Block dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,11 +13,33 @@ const { pathname } = req.nextUrl;
 ///----------------------------------------------------------------
 // Check in case of subrequest
 ///----------------------------------------------------------------
+const path = req.nextUrl.pathname;
+console.log('pathname', path);
+
+if(path.startsWith('/dashboard')){
+  //Get the previous path
+  console.log('doing /dashboard');
+  
+  const referrer = req.headers.get("referer") || "";
+  const referrerUrl = referrer ? new URL(referrer) : null;
+  const referrerPAth = referrerUrl?.pathname || "";
+  console.log(`access to ${path} from referrer ${referrerPAth}`);
+  if (referrerPAth === '/'){
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+}
+//
+const header = req.headers;
+const isSubRequest = header.get('x-middleware-subrequest');
 if (isSubRequest) {
-  const origin = header.origin || header.referer;
+  const origin = header.get('origin') || header.get('referer');
+  console.log('origin at middlewre', origin);
+  
   const url = process.env.NEXT_PUBLIC_api_url;
 
-  if (!origin || !origin.Is(url)) {
+  if (!origin || origin != url) {
     return NextResponse.json({ status: 403, error: 'Unauthorized request' });
 }}
 
