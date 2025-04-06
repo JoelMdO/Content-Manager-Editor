f34e1869552659812a7d0e391988b5b1aa4ef790
JoelMdO <joel.montesdeoca@gmail.com>
2025-04-06 16:06:59 +0300

    <fix>[log_out]: margin correction

--- src/components/buttons/logout_buttons.tsx
+++ src/components/buttons/logout_buttons.tsx
@@ -4,12 +4,34 @@ import callHub from "../../services/api/call_hub";
 import { useRouter } from "next/navigation";
 import errorAlert from "../alerts/error";
 
+interface LogoButtonProps {
+    type?: string;
+}
 //
-const LogOutButton: React.FC<any> = () => {
+const LogOutButton: React.FC<LogoButtonProps> = ({type}) => {
     ///========================================================
     // To log out the user.
     ///========================================================
     //
+        let widthSet = 80;
+        let heightSet = 80;
+        let mt = "md:mt-auto";
+        let mb = "md:mb-14";
+    switch(type){
+
+        case "playbook":
+            widthSet = 50;
+            heightSet = 60;
+            mt = "md:mt-0";
+            mb = "md:mb-0";
+            break;
+        default:
+            widthSet = widthSet;
+            heightSet = heightSet;
+            mt = mt;
+            mb = mb;
+            break;
+    };
     // States
     const [isClicked, setIsClicked] = useState(false);
     const icon="/door_exit.png";
@@ -35,15 +57,19 @@ const LogOutButton: React.FC<any> = () => {
     return ( 
         <>  <button
             type= "button"
-            className = "md:mt-auto md:mb-14"
+            className = {`${mt} ${mb}`}
             onClick={() => {handleLogout(); setIsClicked(true);}}>
             {/* Mobile Icon */}
                 <Image src={icon_mobile} className="block md:hidden cursor-pointer" width={50} height={60} alt="logout-button"/>
             {/* Desktop Icon */}
-                <Image src={icon} className="hidden md:block cursor-pointer" width={70} height={80} alt="logout-button"/>
+                <Image src={icon} className="hidden md:block cursor-pointer" width={widthSet} height={heightSet} alt="logout-button"/>
         </button>
+        {type == "playbook" ? null : (
+        <>
         <span className="hidden md:inline">{isClicked ? "Bye!" : "Logout"}</span>
         <span className="md:hidden">{isClicked ? "Bye!" : ""}</span>
+        </>
+        )}
         </>
     );
 }
