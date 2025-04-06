b1e7f997f48950144b1cc8411bbfb424d0c4f14c
JoelMdO <joel.montesdeoca@gmail.com>
2025-04-06 16:39:00 +0300

    <fix>[home]: UI responsive for mobile

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,14 +2,10 @@
 import React from "react"
 import LogOutButton from "../../components/buttons/logout_buttons";
 import LogoButton from "@/components/buttons/logo_button";
-import { useRouter } from "next/navigation";
 import InopButton from "@/components/buttons/inop_button";
 import RouteButton from "@/components/buttons/routes_button";
 
 const Home: React.FC = () => {
-    //
-    const url = process.env.NEXT_PUBLIC_url_api;
-    const router = useRouter();
     //
     return (  
         <>
@@ -21,6 +17,7 @@ const Home: React.FC = () => {
       </aside>
       {/* Menu Mobile*/}
       <nav className="md:hidden w-full h-20vh bg-gray-800 text-white flex justify-around p-2 flex-row fixed">
+        <LogoButton type="home" />
         <LogOutButton />
       </nav>
 
@@ -28,15 +25,15 @@ const Home: React.FC = () => {
       <main className="flex-1 p-4 pt-[20vh] md:pt-2 md:w-[75%] overflow-y-auto min-h-screen items-center">
          <h1 className="text-white pt-5 font-bold font-roboto text-xl">Home</h1>
          <div className="flex flex-col justify-center items-center h-[90%] w-full">
-         <div className="flex flex-col w-[50%] h-[30%] bg-transparent border-slate-500 border mt-5">
+         <div className="flex flex-col w-[80%] md:w-[50%] h-[30%] bg-transparent border-slate-500 border md:mt-5 mt-2">
             <h1 className="text-white pt-6 pl-2 text-xl font-roboto">Playbook</h1>
             <p className="text-gray-500 text-xs pl-2">Your quick access to software engineer code solutions.</p>
-            <div className="flex flex-row self-center pt-2">
+            <div className="flex flex-row self-center pt-2 gap-x-4 ">
             <RouteButton type="playbook" />
             <RouteButton type="read-playbook" />
          </div>
          </div>
-         <div className="flex flex-col w-[50%] h-[30%] bg-transparent border-slate-500 border mt-8 align-middle">
+         <div className="flex flex-col w-[80%] md:w-[50%] h-[30%] bg-transparent border-slate-500 border md:mt-8 mt-3 align-middle">
             <h1 className="text-white pt-6 pl-2 text-xl font-roboto">CMS</h1>
             <p className="text-gray-500 text-xs pl-2">Write an article or continue working on one.</p>
             <div className="flex flex-row self-center pt-2">
