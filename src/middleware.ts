6011227162edee2646208c5c4cb92527bfa72fbd
JoelMdO <joel.montesdeoca@gmail.com>
2025-04-12 19:05:37 +0300

    <note>[all]: added cypress, middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import rateLimit from './services/api/rate_limit';
 import authMiddleware from './utils/auth_middleware';
 
 export async function middleware(req: any) {
-//
 ///----------------------------------------------------------------
 // Check in case of subrequest
 ///----------------------------------------------------------------
@@ -14,17 +13,15 @@ const database_url = process.env.NEXT_PUBLIC_databaseURL;
 const database_2_url = process.env.NEXT_PUBLIC_Mongo_uri;
 console.log('pathname', path);
 
-// if(path.startsWith('/dashboard') || path.startsWith('/playbook') || path.startsWith('/read-playbook')) {
-  if(path.startsWith('/dashboard')) {
+if(path.startsWith('/dashboard') || path.startsWith('/playbook') || path.startsWith('/read-playbook')) {
   //Get the previous path
-  console.log('doing /path', path);
   console.log('doing /path', path);
   
   const referrer = req.headers.get("referer") || "";
   const referrerUrl = referrer ? new URL(referrer) : null;
   const referrerPAth = referrerUrl?.pathname || "";
   console.log(`access to ${path} from referrer ${referrerPAth}`);
-  if (referrerPAth === '/'){
+  if (referrerPAth === '/home'){
     rateLimitResponse = await rateLimit(req);
     if (rateLimitResponse.status === 200) {
       return response;
@@ -36,6 +33,8 @@ console.log('pathname', path);
   }
 }
 //
+const header = req.headers;
+const isSubRequest = header.get('x-middleware-subrequest');
 if (isSubRequest) {
   const origin = header.get('origin') || header.get('referer');
   console.log('origin at middlewre', origin);
@@ -54,25 +53,25 @@ if (isSubRequest) {
 ///----------------------------------------------------------------
 ///------ Add headers ----------------
 ///----------------------------------------------------------------
-    // response.headers.set(
-    //   'Content-Security-Policy',
-    //   `
-    //   default-src 'self';
-    //   script-src 'self';
-    //   style-src 'self';
-    //   img-src 'self';
-    //   font-src 'self';
-    //   connect-src 'self' ${database_url} ${database_2_url};
-    //   object-src 'none';
-    //   base-uri 'self';
-    //   form-action 'self';            
-    //   frame-ancestors 'self';
-    //   upgrade-insecure-requests;
-    //   block-all-mixed-content;
-    //   `.replace(/\s{2,}/g, ' ').trim()
-    // );
+    response.headers.set(
+      'Content-Security-Policy',
+      `
+      default-src 'self';
+      script-src 'self';
+      style-src 'self';
+      img-src 'self';
+      font-src 'self';
+      connect-src 'self' ${database_url} ${database_2_url};
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';            
+      frame-ancestors 'self';
+      upgrade-insecure-requests;
+      block-all-mixed-content;
+      `.replace(/\s{2,}/g, ' ').trim()
+    );
     return response;
-};
+}
 
 export const config = {
   matcher: ['/api/post', '/api/save', '/dashboard', '/playbook', '/read-playbook'],
